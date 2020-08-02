1ff8242fb9d6af7e7d53420c68e8b71b96ebd653
Ptasiol <szerszenierro@gmail.com>
2020-08-03 00:33:35 +0200

    cleaning

---
+++ build-dir/CMakeFiles/App.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Box.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Box.cpp.o"
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Camera.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Camera.cpp.o"
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Main.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Main.cpp.o"
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Mesh.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Mesh.cpp.o"
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Model.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Model.cpp.o"
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Move.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Move.cpp.o"
+  "/home/ptasiol/learning/OpenGL/RubiksCube/src/Shader.cpp" "/home/ptasiol/learning/OpenGL/RubiksCube/build-dir/CMakeFiles/App.dir/src/Shader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-dir/CMakeFiles/App.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "App"
+  "App.pdb"
+  "CMakeFiles/App.dir/src/Box.cpp.o"
+  "CMakeFiles/App.dir/src/Camera.cpp.o"
+  "CMakeFiles/App.dir/src/Main.cpp.o"
+  "CMakeFiles/App.dir/src/Mesh.cpp.o"
+  "CMakeFiles/App.dir/src/Model.cpp.o"
+  "CMakeFiles/App.dir/src/Move.cpp.o"
+  "CMakeFiles/App.dir/src/Shader.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
